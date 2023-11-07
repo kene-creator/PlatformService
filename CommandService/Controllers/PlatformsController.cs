a7ed371c1b22f271697a61c5f85d4a6e71ce6b49
Kene <kayyungx@gmail.com>
2023-11-07 13:42:06 +0100

    Add docker file to command service

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlatformService.Controllers;
+
+[Route("api/c/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+        
+    }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Test inbound connection");
+
+        return Ok("inbound test from Platforms controller");
+    }
+}
