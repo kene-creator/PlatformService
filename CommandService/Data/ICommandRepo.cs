e97f0ee80840990c5388b2e143caa15b86b0cc23
Kene <kayyungx@gmail.com>
2023-12-14 23:26:47 +0100

    Add models and repository classes

---
+++ CommandService/Data/ICommandRepo.cs
@@ -0,0 +1,20 @@
+﻿using CommandService.Models;
+
+namespace CommandService.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChanges();
+
+    IEnumerable<Platform> GetALlPlatform();
+
+    void CreatePlatform(Platform plat);
+
+    bool PlatformExists(int platformId);
+
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+
+    Command GetCommand(int platformId, int commandId);
+
+    void CreateCommand(int platformId, Command command);
+}
