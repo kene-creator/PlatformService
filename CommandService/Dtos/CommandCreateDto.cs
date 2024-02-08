7422956673e9ae8671fead96ef0dfde195f9c356
Kene <kayyungx@gmail.com>
2024-02-08 14:20:53 +0100

    create command dto

---
+++ CommandService/Dtos/CommandCreateDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.Dtos
+{
+    public class CommandCreateDto
+    {
+
+        [Required]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
+
+    }
+}
